79f248ff26b5308ee29ad083e77738249a277f2c
andreamil <andrezoqui@gmail.com>
2019-01-03 21:15:52 -0200

    remove unused imports

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,15 +1,9 @@
 import { NgModule } from '@angular/core';
 
 import { PagesComponent } from './pages.component';
-import { DashboardModule } from './dashboard/dashboard.module';
-import { ECommerceModule } from './e-commerce/e-commerce.module';
 import { PagesRoutingModule } from './pages-routing.module';
 import { ThemeModule } from '../@theme/theme.module';
 import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
-import { ProjetosModule } from './projetos/projetos.module';
-import { UsuariosModule } from './usuarios/usuarios.module';
-import { PortaModule } from './porta/porta.module';
-import { EstoqueModule } from './estoque/estoque.module';
 
 const PAGES_COMPONENTS = [
   PagesComponent,
