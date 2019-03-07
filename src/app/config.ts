97bee3abff51892b16235eb11c6d9d30436edd05
LARMPC <larm@larmpc.com>
2019-03-07 15:45:39 -0300

    camera

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,4 +1,4 @@
 export const Config = {
-    BASE_API_URL: 'http://150.162.234.151:8080/',
+    BASE_API_URL: 'http://150.162.234.151:44444/',
     // BASE_API_URL: 'http://larm-backend.herokuapp.com/',
 };
