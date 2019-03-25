2542136d53058ae3f3df6663988fe652ad3851e5
andreamil <andreamil@github.com>
2019-03-25 13:55:53 -0300

    pipes

---
+++ src/app/@theme/pipes/filter.pipe.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform } from '@angular/core';
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+  itemsAnterior:any[]=[];
+  transform(items: any[], searchText: string): any[] {
+    if(!items) return [];
+    if(!searchText) return items;
+searchText = searchText.toLowerCase();
+    let returnItems= items.filter( it => {
+      return it.fullName.toLowerCase().includes(searchText)
+          ||(it.matricula&&it.matricula.includes(searchText))
+          ||(it.role.filter(r=>r.includes(searchText)).length);
+    });
+    if(returnItems.length){
+      this.itemsAnterior=returnItems;
+      return returnItems;
+    }
+    else {
+      return this.itemsAnterior;
+    }
+   }
+}
