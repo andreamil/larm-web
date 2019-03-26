2a6d999e87f18d47f8031a75388bedbf7173de5e
andreamil <andreamil@github.com>
2019-03-26 18:22:34 -0300

    troca de tema

---
+++ src/app/paginas/porta/uso-laboratorio/uso-laboratorio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-uso-laboratorio',
+  templateUrl: './uso-laboratorio.component.html',
+  styleUrls: ['./uso-laboratorio.component.scss'],
+})
+export class UsoLaboratorioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
