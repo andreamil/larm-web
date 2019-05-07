6a7fff59b8a436cb34a0a57934ff48bdc3e43881
andreamil <andreamil@github.com>
2019-05-07 00:55:10 -0300

    localization dates

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,6 +1,6 @@
 export const Config = {
-    // BASE_API_URL: '',
-    BASE_API_URL: 'http://150.162.234.151:45454/',
+    BASE_API_URL: '',
+    // BASE_API_URL: 'http://150.162.234.151:45454/',
     // BASE_API_URL: 'http://192.168.1.206:4444/',
     // BASE_API_URL: 'http://larm-backend.herokuapp.com/',
 };
