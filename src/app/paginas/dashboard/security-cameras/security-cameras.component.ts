2ca715840d1acdfe129dea2ce72287bb77c87618
Andre Luis da Rosa de Lima <andre.lrl@ufsc.br>
2019-08-31 14:33:32 -0300

    https camera

--- src/app/paginas/dashboard/security-cameras/security-cameras.component.ts
+++ src/app/paginas/dashboard/security-cameras/security-cameras.component.ts
@@ -16,7 +16,7 @@ export class SecurityCamerasComponent {
     for (let i = 0; i < 4; i++) {
       this.cameras[i]={};
       this.cameras[i].title= 'Camera #'+(i+1);
-      this.cameras[i].html= '<iframe id="iframe0" name="iframe0" src="http://150.162.234.21:8888/Camera%200'+(i+1)+'/snapshotfull.php?clickurl=summarysnapshot.php" width="100%" height="100%" frameborder="0" scrolling="no"></iframe>';
+      this.cameras[i].html= '<iframe id="iframe0" name="iframe0" src="https://www.larm.gq:4444/httpsCamera/0'+(i+1)+'" width="100%" height="100%" frameborder="0" scrolling="no"></iframe>';
     }
     this.selectedCamera = this.cameras[0];
   }
